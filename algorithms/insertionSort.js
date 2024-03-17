8b5bd6875724d1ed5ba696a5c5d5052f78f48ac7
Mher Hunanyan <mher.hunanyan2@gmail.com>
2024-03-18 01:15:31 +0400

    .

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -8,9 +8,9 @@ function insertionSort(arr) {
     let j = i - 1;
     while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
-      arr[j] = key;
       --j;
     }
+    arr[j + 1] = key;
   }
 
   return arr;
