8dd1e0f2b41ba010da62a014b2ef2d5135b052d1
Mher Hunanyan <mher.hunanyan2@gmail.com>
2024-04-15 16:57:11 +0400

    .

--- algorithms/searching-algorithms/BinarySearchIterativ.js
+++ algorithms/searching-algorithms/BinarySearchIterativ.js
@@ -5,13 +5,14 @@ export function BinarySearch(arr, target) {
     let mid = left + Math.floor((right - left) / 2);
     if (target === arr[mid]) {
       return mid;
-    } else if (target < arr[mid]) {
+    }
+    if (target < arr[mid]) {
       right = mid - 1;
-    } else if (target > arr[mid]) {
+    } else {
       left = mid + 1;
     }
   }
   return -1;
 }
 
-const arr = [1,2,3,4,5,6,7];
+const arr = [1, 2, 3, 4, 5, 6, 7];
--- algorithms/searching-algorithms/BinarySearchRecursive.js
+++ algorithms/searching-algorithms/BinarySearchRecursive.js
@@ -6,9 +6,10 @@ function BinarySearch(arr, target, left = 0, right = arr.length - 1) {
     let mid = left + Math.floor((right - left) / 2);
     if (target === arr[mid]) {
       return mid;
-    } else if (target < arr[mid]) {
+    }
+    if (target < arr[mid]) {
       return BinarySearch(arr, target, left, (right = mid - 1));
-    } else if (target > arr[mid]) {
+    } else {
       return BinarySearch(arr, target, (left = mid + 1), right);
     }
   }
